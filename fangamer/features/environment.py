ff1bc86b1d4c621a847066e8a924eb998301e647
drewlarso <drewlarso1@gmail.com>
2025-11-15 13:11:53 -0700

    update deploy.yml

--- fangamer/features/environment.py
+++ fangamer/features/environment.py
@@ -1,9 +1,13 @@
 import behave_webdriver
 from behave_webdriver.steps import *
+from selenium.webdriver.chrome.options import Options
 
 
 def before_all(context):
-    context.behave_driver = behave_webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    context.behave_driver = behave_webdriver.Chrome(options=chrome_options)
 
 
 def after_all(context):
